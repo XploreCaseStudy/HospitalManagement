97ff72e22042e8d38742b886461b034db0e3a42f
Akshay-Bali <akshaybali5@gmail.com>
2020-06-30 14:48:36 +0530

    login sign up create patient

---
+++ HMD/forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,SelectField, SubmitField, BooleanField,DateField
+from wtforms.validators import DataRequired,Regexp, Length, Email, EqualTo, ValidationError
+from HMD.models import User,Patient
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    # email = StringField('Email',
+    #                     validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user:
+    #         raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class PatientForm(FlaskForm):
+    ws_pat_name = StringField('Name',validators=[DataRequired(),Regexp('^[A-Za-z]')])
+    ws_adrs = StringField('Address',validators=[DataRequired()])
+    ws_age = StringField('Age',validators=[DataRequired(),Regexp('^[0-9]'), Length(min=3)])
+    ws_doj = DateField('Date Of Joining',format='%Y-%m-%d')
+    HOUR_CHOICES = [('1', 'General'), ('2', 'Semi'), ('3', 'Simple')]
+    ws_rtype = SelectField('Room Type', choices=HOUR_CHOICES)
+    submit = SubmitField('Submit')
---
+++ HMD/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from HMD import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.image_file}')"
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ws_pat_name = db.Column(db.String(100), nullable=False)
+    ws_adrs = db.Column(db.String(100), nullable=False)
+    ws_age = db.Column(db.String(100), nullable=False)
+    ws_doj = db.Column(db.String(100), nullable=False)
+    ws_rtype = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.ws_pat_name}', '{self.ws_rtype}'), '{self.ws_age}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
